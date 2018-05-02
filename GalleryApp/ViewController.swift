483742e69a2aa6e69534c38d9189d00a44c7872c
Petr <xflajs00@stud.fit.vutbr.cz>
2018-05-02 21:27:52 +0200

    Initial Commit

---
+++ GalleryApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GalleryApp
+//
+//  Created by Petr Flajsingr on 02/05/2018.
+//  Copyright © 2018 VUT FIT. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
