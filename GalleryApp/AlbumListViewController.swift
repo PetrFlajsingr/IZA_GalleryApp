fe23e814306f0989d4275c64b622020292f785cf
Petr <xflajs00@stud.fit.vutbr.cz>
2018-05-03 15:35:03 +0200

    gui

---
+++ GalleryApp/AlbumListViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AlbumListViewController.swift
+//  GalleryApp
+//
+//  Created by Petr Flajsingr on 03/05/2018.
+//  Copyright © 2018 VUT FIT. All rights reserved.
+//
+
+import Cocoa
+
+class AlbumListViewController: NSViewController {
+    @IBOutlet var AlbumListArrayController: NSArrayController!
+    
+    @objc var MOC: NSManagedObjectContext {
+        return (NSApplication.shared.delegate as!
+            AppDelegate).persistentContainer.viewContext
+    }
+    
+    @objc var EDI : Bool = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
