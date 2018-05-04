17f505c7aa0b3d2a3bc38399e9201e98e7eaf748
Petr <xflajs00@stud.fit.vutbr.cz>
2018-05-04 08:11:59 +0200

    add imageData do DB

---
+++ GalleryApp/PhotosViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PhotosViewController.swift
+//  GalleryApp
+//
+//  Created by Petr Flajsingr on 04/05/2018.
+//  Copyright © 2018 VUT FIT. All rights reserved.
+//
+
+import Cocoa
+
+class PhotosViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
