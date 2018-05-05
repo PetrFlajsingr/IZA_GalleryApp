95e47040ed3d27a94f7c35c8b377d31fd1f6ca97
Petr <xflajs00@stud.fit.vutbr.cz>
2018-05-05 17:30:40 +0200

    album saving done

--- GalleryApp/AlbumPhotoViewController.swift
+++ GalleryApp/AlbumPhotoViewController.swift
@@ -57,6 +57,68 @@ class AlbumPhotoViewController: NSViewController {
         reloadArrayController()
     }
     
+    @IBAction func SaveAllImages_OnClick(_ sender: NSButton) {
+        let dialog = NSOpenPanel();
+        
+        dialog.title                   = "Choose target folder";
+        dialog.canChooseDirectories    = true;
+        dialog.showsResizeIndicator    = true;
+        dialog.showsHiddenFiles        = false;
+        dialog.canCreateDirectories    = true;
+        dialog.isExtensionHidden       = false;
+        dialog.canChooseFiles          = false;
+        
+        if (dialog.runModal() == NSApplication.ModalResponse.OK) {
+            let result = dialog.url
+            
+            if let url = result {
+                for photoEntity in PhotoArrayController.arrangedObjects as! [PhotoEntity] {
+                    let filepathUrl = URL(string: url.absoluteString + "/" + photoEntity.title! + "." + photoEntity.format!)!
+                    
+                    let imageData : NSData = (photoEntity.imageData! as NSData)
+                    
+                    let image = NSImage(data: imageData as Data)
+                    
+                    let pathExtension = photoEntity.format
+                    
+                    var saveResult : Bool = true
+                    
+                    switch pathExtension{
+                    case "jpg", "jpeg":
+                        saveResult = (image?.jpgWrite(url: filepathUrl))!
+                    case "png":
+                        saveResult = (image?.pngWrite(url: filepathUrl))!
+                    case "bmp":
+                        saveResult = (image?.bmpWrite(url: filepathUrl))!
+                    case "gif":
+                        saveResult = (image?.gifWrite(url: filepathUrl))!
+                    default:
+                        let alert = NSAlert()
+                        
+                        alert.messageText = "Unsupported file format"
+                        alert.alertStyle = NSAlert.Style.critical
+                        alert.addButton(withTitle: "OK")
+                        
+                        alert.runModal()
+                        return
+                    }
+                    
+                    if(!saveResult){
+                        let alert = NSAlert()
+                        
+                        alert.messageText = "The file could not be saved"
+                        alert.alertStyle = NSAlert.Style.critical
+                        alert.addButton(withTitle: "OK")
+                        
+                        alert.runModal()
+                    }
+                }
+            }
+        } else {
+            return
+        }
+    }
+    
     var photoDetailWindow : PhotoDetailWindowController?
     @IBAction func PhotoTable_OnDoubleClick(_ sender: NSTableView) {
         let appDelegate = (NSApplication.shared.delegate as! AppDelegate)
